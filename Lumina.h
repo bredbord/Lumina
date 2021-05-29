e27a771f25c83855729815b0c0e7eb396f71cc35
NyelDuncan110 <larsriver@yahoo.com>
2021-05-28 18:54:31 -0700

    Header

---
+++ Lumina.h
@@ -0,0 +1,52 @@
+// Lumina controller
+// 2021
+// version 1
+
+#ifndef LUMINA_H
+#define LUMINA_H
+
+#include <Arduino.h>
+#include <FastLED.h>
+#include "EEPROM.h"
+
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define MAX_MODE 11
+
+class Lumina {
+    private:
+        int mode, brightness;
+        bool dimmerDirection, EEHoldWrite;
+
+        FastLED* FastLEDPtr;
+
+        // functions
+        void checkMaxMode();
+
+public:
+    Lumina();
+    Lumina(FastLED* ptr);
+    ~Lumina();
+
+    void advanceMode();  // increment mode
+    void decrementMode();
+
+    void setMode(int val);
+    void storeMode();
+
+    int getMode();
+
+};
+
+Lumina::Lumina(/* args */)
+{
+}
+
+Lumina::~Lumina()
+{
+}
+
+
+#endif
